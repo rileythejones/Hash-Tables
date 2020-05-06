20271140efe74b7e370c843e1bf0379f49fa3f88
rileythejones <rileymjones@gmail.com>
2020-05-06 17:17:03 -0600

    collision resolution

--- hash_practice.py
+++ hash_practice.py
@@ -1,21 +1,16 @@
 hash_table_size = 8
-
 hash_table = [None] * hash_table_size 
 
 def myhash(s):
 	str_bytes = s.encode()
-
 	total = 0 
-
 	for b in str_bytes:
 		total += b
 	return total 
 
 def hash_index(s):
 	h = myhash(s)
-
 	return h % hash_table_size
-
 
 def put(key, value):
     index = hash_index(key)
@@ -30,12 +25,10 @@ def delete(key):
     hash_table[index] = None
 
 if __name__ == "__main__":
-
     # print(hash_index("Hello"))
     # print(hash_index("foobar"))
     # print(hash_index("cats"))
     # print(hash_index("beej"))
-
 
     put("Hello", 37)
     put("foobar", 128)
